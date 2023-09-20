66a54ee6aea3a9ca79063e21894c08d15d6ab6e9
Dclasyc <Akarigbooladipo@gmail.com>
2023-09-20 12:21:07 +0100

    H2 database, csrf and frameOptions Config commit

--- src/main/java/com/dclasyc/springboot/myspringwebapp/security/SpringSecurityConfiguration.java
+++ src/main/java/com/dclasyc/springboot/myspringwebapp/security/SpringSecurityConfiguration.java
@@ -2,13 +2,19 @@ package com.dclasyc.springboot.myspringwebapp.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 import java.util.function.Function;
+
+import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
 public class SpringSecurityConfiguration {
@@ -41,4 +47,24 @@ public class SpringSecurityConfiguration {
     public PasswordEncoder passwordEncoder(){
         return new BCryptPasswordEncoder();
     }
+
+    //Protect All Urls
+    //A login form is shown for unauthorized request
+    //CSRF disable
+    //disable Frame options
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
+        //cos we overide the default secutity features with this method, we need to define them again
+        //then we can continue wtih the customized security features we need
+        http.authorizeHttpRequests(auth -> auth.anyRequest().authenticated());
+        http.formLogin(withDefaults());
+        http.csrf(csrf -> csrf.disable() );
+        http.headers(headers -> headers.frameOptions(frameOptions -> frameOptions.sameOrigin()) );
+        
+        return http.build();
+
+    }
+
 }
